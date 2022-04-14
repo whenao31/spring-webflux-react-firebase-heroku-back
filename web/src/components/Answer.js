25c12575d2e1431cbfdc1b0aa0fcf7e222d29b38
whenao31 <whenao86@gmail.com>
2022-04-13 23:40:19 -0500

    Adicionar boton para borrar c/respuesta, falta conexion con el back

--- web/src/components/Answer.js
+++ web/src/components/Answer.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux'
+import Swal from 'sweetalert2';
 
-export const Answer = ({ answer }) => (
-  <aside className="answer">
-    <p>{answer.answer}</p>
-  </aside>
-)
+export const Answer = ({ answer }) => {
+
+  const userId = useSelector((state) => state.auth.uid);
+
+  const dispatch = useDispatch();
+
+  const deleteHanlde = () => {
+
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+          // dispatch(deleteQuestion(id))
+          Swal.fire(
+              'Deleted!',
+              `Your answer '${answer.id}' has been deleted.`,
+              'success'
+          )
+      }
+    })
+  }
+
+  return (
+      <aside className="answer">
+        {
+        answer.userId === userId && 
+        <button
+        className='button right'
+        onClick={deleteHanlde}
+        >Delete</button>
+        }
+        <p>{answer.answer}</p>
+      </aside>
+  )
+}
